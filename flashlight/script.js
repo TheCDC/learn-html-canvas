658d5722031fc9a728ee5000d99304e6bebde52d
Christopher Chen <christoipher.chen@gmail.com>
2019-08-06 11:54:02 -0700

    Improve flicker animation with moving average of markov chain of on/off states

--- flashlight/script.js
+++ flashlight/script.js
@@ -1,49 +1,58 @@
 class Simulation {
-
-    constructor() {
-        this.startTime = (new Date()).getTime();
-        this.on = true;
+  constructor() {
+    this.startTime = new Date().getTime();
+    this.on = true;
+    this.brightness = 0;
+    this.timeStep = 30; //milliseconds
+    this.flickerChance = 1 / 10;
+    // this.freq = 2;
+    this.dBrightness = 0;
+    this.frames = new Array(10).fill(0);
+    this.movingAverage = 0;
+    this.pBrightDark = 0.1;
+    this.pDarkbright = 0.3;
+  }
+  step() {
+    // this.brightness = Math.sin((new Date()).getTime()/(1000/this.freq)) >= 0.5 ? 1 : 0
+    if (this.brightness === 1) {
+      if (Math.random() < this.pBrightDark) {
         this.brightness = 0;
-        this.timeStep = 30; //milliseconds
-        this.flickerChance = 1 / 10;
-        // this.freq = 2;
-        this.dBrightness = 0;
-    }
-    step() {
-        this.brightness = Math.sin((new Date()).getTime()/(1000/this.freq)) >= 0.5 ? 1 : 0
-        if (Math.random() <= this.flickerChance) {
-            this.brightness = 0;
-        }else{
+      }
+    } else if (this.brightness === 0) {
+        if (Math.random() < this.pDarkbright ){
             this.brightness = 1;
         }
+    } else {
+      console.error("brightness has invalid value of ", this.brightness);
     }
+    // moving average for brightness
+    this.frames.shift();
+    this.frames.push(this.brightness);
+    this.movingAverage =
+      (arr => arr.reduce((a, b) => a + b, 0))(this.frames) / this.frames.length;
+  }
 }
 
 function drawSimulation(ctx, sim) {
-    ctx.fillStyle = `rgb(0,0,0)`;
-    ctx.fillRect(0, 0, 300, 300)
-    var img = document.getElementById('img-flashlight')
-    ctx.drawImage(img, 150, 150, 100, 50)
-    // console.log(sim.brightness)
-    var b = (sim.brightness + 1) *(0.5*255)
-    ctx.fillStyle = `rgb(${b},${b},${b})`
-    ctx.fillRect(0, 0, 50, 50)
-
-
-
+  ctx.fillStyle = `rgb(0,0,0)`;
+  ctx.fillRect(0, 0, 300, 300);
+  var img = document.getElementById("img-flashlight");
+  ctx.drawImage(img, 150, 150, 100, 50);
+//   console.log(sim.movingAverage);
+  var b = sim.movingAverage * 255;
+  ctx.fillStyle = `rgb(${b},${b},${b})`;
+  ctx.fillRect(50, 150, 100, 50);
 }
 function startDrawing() {
-    var canvas = document.getElementById("myCanvas");
-    if (canvas.getContext) {
-        var ctx = canvas.getContext("2d");
+  var canvas = document.getElementById("myCanvas");
+  if (canvas.getContext) {
+    var ctx = canvas.getContext("2d");
 
-
-        var sim = new Simulation();
-        // main loop
-        setInterval(() => {
-            sim.step();
-            drawSimulation(ctx, sim);
-        }, sim.timeStep)
-
-    }
+    var sim = new Simulation();
+    // main loop
+    setInterval(() => {
+      sim.step();
+      drawSimulation(ctx, sim);
+    }, sim.timeStep);
+  }
 }
