0f111aa90dba498997db1f89499ad84fa2d78dae
Christopher Chen <christoipher.chen@gmail.com>
2020-11-12 08:44:26 -0800

    Mask light by figures instead of figures by lights

--- spooky-flashlight-figures/sketch.js
+++ spooky-flashlight-figures/sketch.js
@@ -19,10 +19,33 @@ class RandomMover {
     this.height = height;
     this.target = { x: 0, y: 0 };
     this.state = 0;
+    this.speed = 5;
     this.chooseNewTarget();
   }
 
-  update() {}
+  update() {
+    //homing
+    if (this.state === 0) {
+      if (
+        abs(this.x - this.target.x) >= 1 &&
+        abs(this.y - this.target.y) >= 1
+      ) {
+        this.state = 1;
+      }
+      if (abs(this.x - this.target.x) >= this.speed) {
+        this.x = this.target.x;
+      } else {
+        this.x += speed * sign((this.x = this.target.x));
+      }
+      if (abs(this.y - this.target.y) >= this.speed) {
+        this.y = this.target.y;
+      } else {
+        this.y += speed * sign((this.y = this.target.y));
+      }
+    } else if (this.state === 1) {
+        if(random()){}
+    }
+  }
 
   chooseNewTarget() {
     this.target = { x: this.x + random(width), y: this.y + random(height) };
@@ -40,6 +63,7 @@ function preload() {
 }
 
 function setup() {
+    frameRate(30);
   createCanvas(640, 480);
   imageMode(CENTER);
 }
@@ -48,16 +72,17 @@ function draw() {
   // background(255, 255, 0, 255);
   // ===== write to static layers
   ///middle figure layer
+  blendMode(REPLACE);
   layerSarah.imageMode(CORNERS);
-  layerSarah.background(64, 0, 0);
+  //   layerSarah.background(64, 0, 0);
   layerSarah.image(imgSarah, 0, 0);
   ///left figure layer
   layerJoe.imageMode(CORNERS);
-  layerJoe.background(0, 64, 0);
+  //   layerJoe.background(0, 64, 0);
   layerJoe.image(imgJoe, 0, 0);
   ///right figure layer
   layerWyatt.imageMode(CORNERS);
-  layerWyatt.background(0, 0, 64);
+  //   layerWyatt.background(0, 0, 64);
   layerWyatt.image(imgWyatt, 0, 0);
 
   ///flashlight spot 1 layer
@@ -69,9 +94,9 @@ function draw() {
 
   //blend middle figure and flashlight spot 1
 
-  let maskedSarah = maskGraphicsByGraphics(layerSarah, layerFlashlightSarah);
-  let maskedJoe = maskGraphicsByGraphics(layerJoe, layerFlashlightJoe);
-  let maskedWyatt = maskGraphicsByGraphics(layerWyatt, layerFlashlightWyatt);
+  let maskedSarah = maskGraphicsByGraphics(layerFlashlightSarah, layerSarah);
+  let maskedJoe = maskGraphicsByGraphics(layerFlashlightJoe, layerJoe);
+  let maskedWyatt = maskGraphicsByGraphics(layerFlashlightWyatt, layerWyatt);
 
   imageMode(CORNERS);
   blendMode(REPLACE);
