9813d34890575f9644aba14e72aff192800953c3
Christopher Chen <christoipher.chen@gmail.com>
2020-11-11 21:48:17 -0800

    Implemented the layers and blending TODO: fix weird lights blending

--- spooky-flashlight-figures/sketch.js
+++ spooky-flashlight-figures/sketch.js
@@ -1,23 +1,30 @@
 let img;
 let imgMask;
-let layerFigureMiddle;
-let layerFlashlight1;
-let layerFlashlight2;
-let layerFlashlight3;
+let layerSarah;
+let lyaerJoe;
+let LayerWyatt;
+let layerFlashlightSarah;
+let layerFlashlightJoe;
+let layerFlashlightWyatt;
 let imgSarah;
 let imgWyatt;
 let imgJoe;
+let imgGradient;
 let HEIGHT = 480;
 let WIDTH = 640;
 
 function preload() {
     imgSarah = loadImage('img/outline-sarah.png');
-    imgWyatt = loadImage('img/outline-wyatt.png');
     imgJoe = loadImage('img/outline-joe.png');
+    imgWyatt = loadImage('img/outline-wyatt.png');
     imgGradient = loadImage('img/gradient.png');
     imgMask = loadImage('img/gradient.png');
-    layerFigureMiddle = createGraphics(WIDTH, HEIGHT);
+    layerSarah = createGraphics(WIDTH, HEIGHT);
+    layerJoe = createGraphics(WIDTH, HEIGHT);
+    layerWyatt = createGraphics(WIDTH, HEIGHT);
     layer2 = createGraphics(WIDTH, HEIGHT);
+
+
 }
 
 function setup() {
@@ -26,24 +33,44 @@ function setup() {
 }
 
 function draw() {
+    // ===== write to static layers
     ///middle figure layer
-    layerFigureMiddle.imageMode(CORNERS);
-    layerFigureMiddle.background(0);
-    layerFigureMiddle.image(imgSarah, 0, 0);
+    layerSarah.imageMode(CORNERS);
+    layerSarah.background(0);
+    layerSarah.image(imgSarah, 0, 0);
+    ///left figure layer
+    layerJoe.imageMode(CORNERS);
+    layerJoe.background(0);
+    layerJoe.image(imgJoe, 0, 0);
+    ///right figure layer
+    layerWyatt.imageMode(CORNERS);
+    layerWyatt.background(0);
+    layerWyatt.image(imgWyatt, 0, 0);
+
     ///flashlight spot 1 layer
-    layerFlashlight1 = createGraphics(WIDTH, HEIGHT);
-    layerFlashlight1.imageMode(CENTER);
-    layerFlashlight1.image(imgGradient, mouseX, mouseY);
+    layerFlashlightSarah = createFlashlightLayer(0, 0);
+    layerFlashlightJoe = createFlashlightLayer(30, 30);
+    layerFlashlightWyatt = createFlashlightLayer(-30, 30);
+
     //blend middle figure and flashlight spot 1
 
-    let masked;
-    (masked = layerFigureMiddle.get()).mask(layerFlashlight1.get());
-    // (masked = layerFlashlight1.get()).mask(layerFigureMiddle.get());
+    let maskedSarah = maskGraphicsByGraphics(layerSarah, layerFlashlightSarah);
+    let maskedJoe = maskGraphicsByGraphics(layerJoe, layerFlashlightJoe);
+    let maskedWyatt = maskGraphicsByGraphics(layerWyatt, layerFlashlightWyatt);
+
     background(0, 0, 0, 255);
     imageMode(CORNERS);
-    image(masked, 0, 0);
+    // blendMode(MULTIPLY);
+    image(maskedJoe, 0, 0);
+    image(maskedWyatt, 0, 0);
+    image(maskedSarah, 0, 0);
 
-    layerFlashlight1.remove();
+    layerFlashlightSarah.remove();
+    layerFlashlightJoe.remove();
+    layerFlashlightWyatt.remove();
+    maskedSarah.reset();
+    maskedJoe.reset();
+    maskedWyatt.reset();
 }
 
 function createFlashlightLayer(xoffset, yoffset) {
@@ -51,4 +78,10 @@ function createFlashlightLayer(xoffset, yoffset) {
     layer.imageMode(CENTER);
     layer.image(imgGradient, mouseX + xoffset, mouseY + yoffset);
     return layer;
+}
+
+function maskGraphicsByGraphics(target, mask) {
+    let masked = target.get();
+    masked.mask(mask.get())
+    return masked;
 }
