1e24afe97282b622ca880bbe523c784c4b5b48e2
Christopher Chen <christoipher.chen@gmail.com>
2020-11-11 23:47:03 -0800

    Offset lights on foreground figures for a parallax/3d effect

--- spooky-flashlight-figures/sketch.js
+++ spooky-flashlight-figures/sketch.js
@@ -12,95 +12,93 @@ let HEIGHT = 480;
 let WIDTH = 640;
 
 class RandomMover {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.target = { x: 0, y: 0 };
+    this.state = 0;
+    this.chooseNewTarget();
+  }
 
-    constructor(x, y, width, height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.target = { x: 0, y: 0 };
-        this.state = 0;
-        this.chooseNewTarget();
+  update() {}
 
-    }
-
-    update() {
-
-    }
-
-    chooseNewTarget() {
-        this.target = { x: this.x + random(width), y: this.y + random(height) };
-    }
+  chooseNewTarget() {
+    this.target = { x: this.x + random(width), y: this.y + random(height) };
+  }
 }
 
 function preload() {
-    imgSarah = loadImage('img/outline-sarah.png');
-    imgJoe = loadImage('img/outline-joe.png');
-    imgWyatt = loadImage('img/outline-wyatt.png');
-    imgGradient = loadImage('img/gradient.png');
-    layerSarah = createGraphics(WIDTH, HEIGHT);
-    layerJoe = createGraphics(WIDTH, HEIGHT);
-    layerWyatt = createGraphics(WIDTH, HEIGHT);
+  imgSarah = loadImage("img/outline-sarah.png");
+  imgJoe = loadImage("img/outline-joe.png");
+  imgWyatt = loadImage("img/outline-wyatt.png");
+  imgGradient = loadImage("img/gradient.png");
+  layerSarah = createGraphics(WIDTH, HEIGHT);
+  layerJoe = createGraphics(WIDTH, HEIGHT);
+  layerWyatt = createGraphics(WIDTH, HEIGHT);
 }
 
 function setup() {
-    createCanvas(640, 480);
-    imageMode(CENTER);
+  createCanvas(640, 480);
+  imageMode(CENTER);
 }
 
 function draw() {
-    // background(255, 255, 0, 255);
-    // ===== write to static layers
-    ///middle figure layer
-    layerSarah.imageMode(CORNERS);
-    layerSarah.background(64, 0, 0);
-    layerSarah.image(imgSarah, 0, 0);
-    ///left figure layer
-    layerJoe.imageMode(CORNERS);
-    layerJoe.background(0, 64, 0);
-    layerJoe.image(imgJoe, 0, 0);
-    ///right figure layer
-    layerWyatt.imageMode(CORNERS);
-    layerWyatt.background(0, 0, 64);
-    layerWyatt.image(imgWyatt, 0, 0);
+  // background(255, 255, 0, 255);
+  // ===== write to static layers
+  ///middle figure layer
+  layerSarah.imageMode(CORNERS);
+  layerSarah.background(64, 0, 0);
+  layerSarah.image(imgSarah, 0, 0);
+  ///left figure layer
+  layerJoe.imageMode(CORNERS);
+  layerJoe.background(0, 64, 0);
+  layerJoe.image(imgJoe, 0, 0);
+  ///right figure layer
+  layerWyatt.imageMode(CORNERS);
+  layerWyatt.background(0, 0, 64);
+  layerWyatt.image(imgWyatt, 0, 0);
 
-    ///flashlight spot 1 layer
-    layerFlashlightSarah = createFlashlightLayer(0, 0);
-    layerFlashlightJoe = createFlashlightLayer(-25, 50);
-    layerFlashlightWyatt = createFlashlightLayer(25, 50);
+  ///flashlight spot 1 layer
+  var yOffset = (300 * (mouseY - HEIGHT / 2)) / 480;
+  var xOffset = (200 * abs(mouseX - WIDTH / 2)) / (WIDTH / 2);
+  layerFlashlightSarah = createFlashlightLayer(0, 0);
+  layerFlashlightJoe = createFlashlightLayer(-xOffset, yOffset);
+  layerFlashlightWyatt = createFlashlightLayer(xOffset, yOffset);
 
-    //blend middle figure and flashlight spot 1
+  //blend middle figure and flashlight spot 1
 
-    let maskedSarah = maskGraphicsByGraphics(layerSarah, layerFlashlightSarah);
-    let maskedJoe = maskGraphicsByGraphics(layerJoe, layerFlashlightJoe);
-    let maskedWyatt = maskGraphicsByGraphics(layerWyatt, layerFlashlightWyatt);
+  let maskedSarah = maskGraphicsByGraphics(layerSarah, layerFlashlightSarah);
+  let maskedJoe = maskGraphicsByGraphics(layerJoe, layerFlashlightJoe);
+  let maskedWyatt = maskGraphicsByGraphics(layerWyatt, layerFlashlightWyatt);
 
-    imageMode(CORNERS);
-    blendMode(REPLACE);
-    background(0);
-    blendMode(ADD);
-    image(maskedWyatt, 0, 0);
-    image(maskedSarah, 0, 0);
-    image(maskedJoe, 0, 0);
+  imageMode(CORNERS);
+  blendMode(REPLACE);
+  background(0);
+  blendMode(ADD);
+  image(maskedWyatt, 0, 0);
+  image(maskedSarah, 0, 0);
+  image(maskedJoe, 0, 0);
 
-    layerFlashlightSarah.remove();
-    layerFlashlightJoe.remove();
-    layerFlashlightWyatt.remove();
-    maskedSarah.reset();
-    maskedJoe.reset();
-    maskedWyatt.reset();
+  layerFlashlightSarah.remove();
+  layerFlashlightJoe.remove();
+  layerFlashlightWyatt.remove();
+  maskedSarah.reset();
+  maskedJoe.reset();
+  maskedWyatt.reset();
 }
 
 function createFlashlightLayer(xoffset, yoffset) {
-    let layer = createGraphics(WIDTH, HEIGHT);
-    layer.imageMode(CENTER);
-    layer.background(0, 0, 0, 64)
-    layer.image(imgGradient, mouseX + xoffset, mouseY + yoffset);
-    return layer;
+  let layer = createGraphics(WIDTH, HEIGHT);
+  layer.imageMode(CENTER);
+  layer.background(0, 0, 0, 64);
+  layer.image(imgGradient, mouseX + xoffset, mouseY + yoffset);
+  return layer;
 }
 
 function maskGraphicsByGraphics(target, mask) {
-    let masked = target.get();
-    masked.mask(mask.get())
-    return masked;
+  let masked = target.get();
+  masked.mask(mask.get());
+  return masked;
 }
